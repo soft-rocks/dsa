62a5fbea255080cb3a2927efab3a3acb4b41749f
soft-rocks <hamza@soft.rocks>
2026-05-06 23:49:47 +0800

    Time: 0 ms (100.00%) | Memory: 10.3 MB (89.47%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.go
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.go
@@ -1,14 +1,14 @@
 func climbStairs(n int, costs []int) int {
-    dp := make([]int, n+1)
-    dp[0] = 0 
-    for j := 1; j <= n; j++ {
-        dp[j] = math.MaxInt // infinite
-        for k := 1; k <= 3 && j-k >= 0; k++ {
-            i := j-k // i = previous step (j-k), skip if invalid (i < 0)
-            if i >= 0 {
-                dp[j] = min(dp[j], dp[i] + costs[j-1] + k*k)
-            }
-        }
-    }
-    return dp[n]
+	a, b, c := 0, 0, 0
+	for _, cost := range costs {
+		a, b, c = b, c, cost+min(a+9, min(b+4, c+1))
+	}
+	return c
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
