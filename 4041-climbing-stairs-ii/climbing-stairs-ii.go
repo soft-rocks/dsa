10b1e5b25d22d7cf62dcdbc36ad95dc501f45c07
soft-rocks <hamza@soft.rocks>
2026-05-07 10:20:07 +0800

    Time: 5 ms (94.74%) | Memory: 10.2 MB (94.74%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.go
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.go
@@ -1,7 +1,7 @@
 func climbStairs(n int, costs []int) int {
-    a,b,c:= 0,0,0
+    a, b, c := 0, 0, 0
     for _, cost := range costs {
-        next := cost + min(a + 9, b + 4, c + 1)
+        next := min(a + 9, b + 4, c + 1) + cost
         a = b
         b = c
         c = next
