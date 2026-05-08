b6c096765993b15e3b467a9acdf5f84e84487386
soft-rocks <hamza@soft.rocks>
2026-05-08 11:06:13 +0800

    Time: 0 ms (100.00%) | Memory: 4 MB (60.67%) - LeetSync

--- 198-house-robber/house-robber.go
+++ 198-house-robber/house-robber.go
@@ -1,12 +1,13 @@
 func rob(nums []int) int {
-    dp := make([]int, len(nums))
     if len(nums) == 1 {
         return nums[0]
     }
-    dp[0] = nums[0]
-    dp[1] = max(nums[0], nums[1])
+    f1 := nums[0]
+    f2 := max(nums[0], nums[1])
     for i := 2; i < len(nums); i++ {
-        dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        next := max(nums[i] + f1, f2)
+        f1 = f2
+        f2 = next
     }
-    return dp[len(dp) - 1]
+    return f2
 }
