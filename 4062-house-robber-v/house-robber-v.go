8499c9768e4e882711320d2258a533225cff19aa
soft-rocks <hamza@soft.rocks>
2026-05-10 21:26:57 +0800

    Time: 12 ms (41.18%) | Memory: 13 MB (64.71%) - LeetSync

--- 4062-house-robber-v/house-robber-v.go
+++ 4062-house-robber-v/house-robber-v.go
@@ -1,21 +1,22 @@
 func rob(nums []int, colors []int) int64 {
-    if len(nums) == 0 {
+    n := len(nums)
+    if n == 0 {
         return 0
     }
-    if len(nums) == 1 {
+    if n == 1 {
         return int64(nums[0])
     }
-    robs := make([]int, len(nums))
-    skip := make([]int, len(nums))
+    robs := make([]int, n)
+    skip := make([]int, n)
     robs[0] = nums[0]
     skip[0] = 0
-    for i := 1; i < len(nums); i++ {
+    for i := 1; i < n; i++ {
         skip[i] = max(robs[i-1], skip[i-1])
         if colors[i] != colors[i-1] {
-            robs[i] = max(robs[i-1], skip[i-1]) + nums[i]
+            robs[i] = nums[i] + max(robs[i-1], skip[i-1])
         } else {
             robs[i] = skip[i-1] + nums[i]
         }
     }
-    return int64(max(robs[len(nums)-1], skip[len(nums)-1]))
+    return int64(max(robs[n-1], skip[n-1]))
 }
