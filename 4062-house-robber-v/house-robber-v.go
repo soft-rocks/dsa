e4a9862d280246241e023859393ee9d7d40f6d4e
soft-rocks <hamza@soft.rocks>
2026-05-10 16:43:34 +0800

    Time: 12 ms (41.18%) | Memory: 14.7 MB (47.06%) - LeetSync

--- 4062-house-robber-v/house-robber-v.go
+++ 4062-house-robber-v/house-robber-v.go
@@ -1,31 +1,43 @@
 func rob(nums []int, colors []int) int64 {
-    var res int64
-    var lastEvenMoney, lastOddMoney, evenMoney, oddMoney int64
-    curColor := 0
-    isEven := true
-    for i, color := range colors {
-        money := nums[i]
-        if color != curColor {
-            res += max(evenMoney, oddMoney)
-            curColor = color
-            evenMoney = int64(money)
-            oddMoney, lastEvenMoney, lastOddMoney = 0,0,0
-            isEven = true
-        } else {
-            isEven = !isEven
-            if isEven {
-                lastEvenMoney, evenMoney = evenMoney, max(evenMoney + int64(money), lastOddMoney + int64(money))
-            } else {
-                lastOddMoney, oddMoney = oddMoney, max(oddMoney + int64(money), lastEvenMoney + int64(money))
-            }
-        }
-    }
-    res += max(evenMoney, oddMoney)
-    return res
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return int64(nums[0])
+	}
+
+	// rob[i] 表示搶第 i 間房的最大收益
+	// skip[i] 表示不搶第 i 間房的最大收益
+	rob := make([]int64, n)
+	skip := make([]int64, n)
+
+	// 初始狀態：第 0 間房
+	rob[0] = int64(nums[0])
+	skip[0] = 0
+
+	for i := 1; i < n; i++ {
+		// 情況 1：如果不搶第 i 間房
+		// 最大收益就是前一間房「搶」或「不搶」中的較大值
+		skip[i] = max(rob[i-1], skip[i-1])
+
+		// 情況 2：如果搶第 i 間房
+		if colors[i] != colors[i-1] {
+			// 如果顏色不同，前一間搶不搶都沒關係
+			rob[i] = max(rob[i-1], skip[i-1]) + int64(nums[i])
+		} else {
+			// 如果顏色相同，前一間一定不能搶，只能接在 skip[i-1] 後面
+			rob[i] = skip[i-1] + int64(nums[i])
+		}
+	}
+
+	// 最終結果是最後一間房「搶」或「不搶」的最大值
+	return max(rob[n-1], skip[n-1])
 }
+
 func max(a, b int64) int64 {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
